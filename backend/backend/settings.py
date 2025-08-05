a60084c25dcb1c9021efc740ee000b02b0defdef
songjju <ssj4358@naver.com>
2025-08-05 10:14:34 +0000

    SECRET_KEY 문제 해결

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,7 +31,7 @@ if env_path.exists():
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY') or env('SECRET_KEY', default='fallback-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
